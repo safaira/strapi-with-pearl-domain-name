9b57704640c05a263ef1f34e5c6ba726d3280eef
saniya <sani2mansuri@gmail.com>
2024-06-23 14:32:59 +0530

    file updated

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,34 +1,14 @@
-resource "aws_instance" "strapi-ec2" {
+resource "aws_instance" "SaniyaStrapiEC2" {
   ami                         = var.ami
-  instance_type               = "t2.small"
+  instance_type               = "t2.medium"
   vpc_security_group_ids      = [aws_security_group.strapiEC2-sg.id]
 
   subnet_id                   = aws_subnet.subnet1.id
 
   key_name                    = "k8-key-pair"
   associate_public_ip_address = true
-  user_data                      = <<-EOF
-                                  #!/bin/bash
-                                  sudo apt update
-                                  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-                                  export NVM_DIR="$HOME/.nvm"
-                                  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-                                  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-                                  sudo chmod 764 ~/.nvm/nvm.sh
-                                  ~/.nvm/nvm.sh
-                                  nvm install node && sudo apt install -y npm
-                                  nvm install 18.0
-                                  nvm use 18
-                                  sudo apt update -y
-                                  echo -e "skip/n" | npx create-strapi-app@latest saniya-strapi-project --quickstart
-                                  npm install pm2 -g
-                                  cd saniya-strapi-project
-                                  echo "const strapi = require('@strapi/strapi');
-                                  strapi().start();" > server.js
-                                  pm2 start server.js  
-                                  pm2 start npm --name strapi -- run start
-                                  sleep 360                            
-                                  EOF         
+  user_data                      = "${file("user_data_strapi.sh")}"       
+  
   tags = {
     Name = "StrapiEC2"
   }
