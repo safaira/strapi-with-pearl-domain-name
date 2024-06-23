6422f85aca64b550c6122442c94df0a0e1729148
saniya <sani2mansuri@gmail.com>
2024-06-23 15:08:22 +0530

    file updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.54.1"
+      version = "5.55.0"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = ap-south-1
 }
