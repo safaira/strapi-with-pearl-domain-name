23c3a9a8b2a85448afa6011b99f527de6ba8cbad
Saniya Shaikh <107477541+safaira@users.noreply.github.com>
2024-06-24 10:27:42 +0530

    Update user_data_strapi.sh

--- terraform/user_data_strapi.sh
+++ terraform/user_data_strapi.sh
@@ -1,3 +1,4 @@
+<<EOF
 #!/bin/bash
 sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -20,3 +21,4 @@ sleep 100
 # pm2 start npm --name strapi -- run start 
 # pm2 monitor && pm2 startup                           
 # sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.0.0/bin /home/ubuntu/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+EOF
