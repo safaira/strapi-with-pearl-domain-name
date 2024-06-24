c79eb03d88819268f5323f28093ad2d516dc505d
saniya <sani2mansuri@gmail.com>
2024-06-24 09:32:28 +0530

    file updated

---
+++ ec2.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "SaniyaStrapiEC2" {
+  ami                         = var.ami
+  instance_type               = "t2.medium"
+  vpc_security_group_ids      = [aws_security_group.strapiEC2-sg.id]
+
+  subnet_id                   = aws_subnet.subnet1.id
+
+  key_name                    = "k8-key-pair"
+  associate_public_ip_address = true
+  user_data                   = "${file("user_data_strapi.sh")}"       
+  
+  tags = {
+    Name = "StrapiEC2"
+  }
+}
+
+resource "aws_security_group" "strapiEC2-sg" {
+  vpc_id      = aws_vpc.strapi_vpc.id
+  description = "Security Group for Strapi Application"
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = "1337"
+    to_port     = "1337"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "Strapi-sg"
+  }
+
+}
+
+
+
---
+++ user_data_strapi.sh
@@ -0,0 +1,24 @@
+<<EOF
+#!/bin/bash
+sudo apt update
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo chmod 764 ~/.nvm/nvm.sh
+~/.nvm/nvm.sh
+nvm install node && sudo apt install -y npm
+nvm install 18.0
+nvm use 18
+sudo apt update -y
+echo -e "skip/n" | npx create-strapi-app@latest saniya-strapi-project --quickstart
+npm install pm2 -g
+cd saniya-strapi-project
+echo "const strapi = require('@strapi/strapi');
+strapi().start();" > server.js
+pm2 start server.js  
+# pm2 start npm --name strapi -- run start 
+# pm2 monitor && pm2 startup                           
+EOF  
+
+# sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.0.0/bin /home/ubuntu/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
