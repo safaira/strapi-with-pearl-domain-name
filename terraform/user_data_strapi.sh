3c9cf8eee3ce772d27419cc43d2de0771ff46c22
Saniya Shaikh <107477541+safaira@users.noreply.github.com>
2024-06-25 09:06:42 +0530

    Update user_data_strapi.sh

--- terraform/user_data_strapi.sh
+++ terraform/user_data_strapi.sh
@@ -1,12 +1,22 @@
-                                #!/bin/bash
-                                sudo apt update
-                                curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
-                                sudo bash -E nodesource_setup.sh
-                                sudo apt update && sudo apt install nodejs -y
-                                sudo npm install -g yarn && sudo npm install -g pm2
-                                echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
-                                cd simple-strapi
-                                echo "const strapi = require('@strapi/strapi');
-                                strapi().start();" > server.js
-                                pm2 start server.js
-                                sleep 360
+#!/bin/bash
+sudo apt update -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo chmod 764 ~/.nvm/nvm.sh
+~/.nvm/nvm.sh
+nvm install node && sudo apt install -y npm
+nvm install 18.0
+nvm use 18
+sudo apt update -y 
+sudo npm install -g yarn && sudo npm install -g pm2
+echo -e "skip/n" | sudo npx create-strapi-app@latest saniya-strapi-project --quickstart
+cd saniya-strapi-project
+echo "const strapi = require('@strapi/strapi');
+strapi().start();" > server.js
+pm2 start server.js
+sleep 360
+# pm2 start npm --name strapi -- run start
+# pm2 monitor && pm2 startup                           
+# sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.0.0/bin /home/ubuntu/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
