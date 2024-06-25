b654e84315f2c46083cd2f9d4c2854720ffce2bf
Saniya Shaikh <107477541+safaira@users.noreply.github.com>
2024-06-25 18:55:47 +0530

    Update user_data_strapi.sh

--- terraform/user_data_strapi.sh
+++ terraform/user_data_strapi.sh
@@ -1,4 +1,3 @@
-<<EOF
 #!/bin/bash
 sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -11,12 +10,13 @@ nvm install node && sudo apt install -y npm
 nvm install 18.0
 nvm use 18
 sudo apt update -y -y && sudo npm install -g pm2
-git clone https://github.com/safaira/Terraform-Strapi-EC2Deployment.git
-cd strapi-ec2
+git clone https://github.com/strapi/strapi.git
+cd strapi
+echo "const strapi = require('@strapi/strapi');
+strapi().start();" > server.js
 pm2 start server.js
 # sleep 100 
 # pm2 start npm --name strapi -- run start 
 # pm2 monitor && pm2 startup                           
-EOF  
 
 # sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.0.0/bin /home/ubuntu/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
