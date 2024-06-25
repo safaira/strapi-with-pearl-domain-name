254a11d8c975e6160a28899f56444487416b835e
Saniya Shaikh <107477541+safaira@users.noreply.github.com>
2024-06-25 16:01:06 +0530

    Update user_data_strapi.sh

--- terraform/user_data_strapi.sh
+++ terraform/user_data_strapi.sh
@@ -1,23 +1,16 @@
-#!/bin/bash
-sudo apt update -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo chmod 764 ~/.nvm/nvm.sh
-~/.nvm/nvm.sh
-nvm install node && sudo apt install -y npm
-nvm install 18.0
-nvm use 18
-sudo apt update -y 
-sudo npm install -g yarn && sudo npm install -g pm2
-echo -e "skip/n" | sudo npx create-strapi-app@latest strapi --quickstart
-cd strapi
-# sudo su
-# echo "const strapi = require('@strapi/strapi');
-# strapi().start();" > server.js
-pm2 start server.js && pm2 save 
-pm2 restart server.js
-# pm2 start npm --name strapi -- run start
-# pm2 monitor && pm2 startup                           
-# sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.0.0/bin /home/ubuntu/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+    #!/bin/bash
+    sudo apt update -y
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    sudo chmod 764 ~/.nvm/nvm.sh
+    ~/.nvm/nvm.sh
+    nvm install node && sudo apt install -y npm
+    nvm install 18.0
+    nvm use 18
+    sudo apt update -y && sudo npm install -g pm2
+    git clone https://github.com/safaira/strapi-ec2.git
+    cd strapi
+    pm2 start server.js
+    sudo reboot
