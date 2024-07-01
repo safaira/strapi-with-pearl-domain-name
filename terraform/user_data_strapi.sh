9b9106d6f573dea258dcf0adcc57225f6832eaf4
saniya <sani2mansuri@gmail.com>
2024-07-01 18:44:38 +0530

    .sh file updated

--- terraform/user_data_strapi.sh
+++ terraform/user_data_strapi.sh
@@ -1,17 +1,51 @@
-#!/bin/bash
-sudo apt update -y
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-# sudo chmod 764 ~/.nvm/nvm.sh
-# ~/.nvm/nvm.sh
-sudo apt install nodejs -y && sudo apt install -y npm
-sudo apt update -y && sudo npm install -g pm2
-cd /srv/
-git clone https://github.com/safaira/strapi.git
-cd strapi/
-pm2 start npm --name strapi -- run start
-sudo pm2 startup systemd
+# #!/bin/bash
+# sudo apt update -y && sudo apt intall docker.io -y
+# git clone https://github.com/safaira/strapi.git
+# cd strapi/
 
-# sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.0.0/bin /home/ubuntu/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+# # #build strapi image from dockerfile
+# # docker build . -t strapi:npm17
+
+# # Log in to Docker Hub to pull the image
+# echo $var.DOCKERHUB_PASSWORD | docker login -u $var.DOCKERHUB_USERNAME --password-stdin
+
+# # # tag and push the docker image
+# # docker tag strapi:latest saniyashaikh/strapi:npm17
+# # docker push saniyashaikh/strapi:npm17
+
+# # run container in detached mode
+# docker run -d --name strapi-app -p 1337:1337 saniyashaikh/strapi-app:npm17
+
+
+
+
+# # sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.0.0/bin /home/ubuntu/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+
+
+#!/bin/bash
+# Update the package index
+sudo apt update -y
+
+# Install Docker
+sudo apt install docker.io -y
+
+# Start Docker service
+sudo service docker start
+sudo usermod -a -G docker ubuntu
+
+# Install AWS CLI and jq
+sudo apt install -y aws-cli jq
+
+# Retrieve Docker Hub credentials from AWS Secrets Manager
+CREDENTIALS=$(aws secretsmanager get-secret-value --secret-id dockerhub_credentials --query SecretString --output text)
+DOCKERHUB_USERNAME=$(echo $CREDENTIALS | jq -r '.username')
+DOCKERHUB_PASSWORD=$(echo $CREDENTIALS | jq -r '.password')
+
+# Log in to Docker Hub
+echo $DOCKERHUB_PASSWORD | sudo docker login -u $DOCKERHUB_USERNAME --password-stdin
+
+# Pull the Docker image from the private repository
+docker pull saniyashaikh/strapi-app:npm17
+
+# Run the Docker container
+docker run -d --name my_app_container -p 1337:1337 saniyashaikh/strapi-app:npm17
