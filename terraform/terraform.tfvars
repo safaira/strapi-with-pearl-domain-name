614b1cba97f3b501fc399ac77693b1a5bb6b367f
saniya <sani2mansuri@gmail.com>
2024-07-02 18:00:28 +0530

    add variables

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+aws_region      = "ap-south-1"
+route53_zone_id = "Z06607023RJWXGXD2ZL6M"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,11 @@
-variable "region" {
-  default = "ap-south-1"
+variable "aws_region" {
+  description = "The Name of the regin"
+  type        = string
 }
 
+variable "route53_zone_id" {
+  description = "The ID of the Route 53 hosted zone"
+  type        = string
+}
+
+Z06607023RJWXGXD2ZL6M
