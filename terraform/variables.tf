2f0e199f1450a6c03c25c1835b6a3737d99b4060
saniya <sani2mansuri@gmail.com>
2024-07-02 18:54:26 +0530

    variable assigned

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,15 @@ variable "route53_zone_id" {
   description = "The ID of the Route 53 hosted zone"
   type        = string
 }
+
+# variable in workflow file should be assigned here.
+variable "dockerhub_username" {
+  description = "Docker Hub Username"
+  type        = string
+}
+
+variable "dockerhub_password" {
+  description = "Docker Hub Password"
+  type        = string
+  sensitive   = true
+}
