d54c1fc878a1d6feb225d1e806effb9df33124b7
saniya <sani2mansuri@gmail.com>
2024-07-02 18:01:25 +0530

    add variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  description = "The Name of the regin"
+  description = "The Name of the region"
   type        = string
 }
 
@@ -7,5 +7,3 @@ variable "route53_zone_id" {
   description = "The ID of the Route 53 hosted zone"
   type        = string
 }
-
-Z06607023RJWXGXD2ZL6M
